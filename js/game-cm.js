7dd5088715d4dd7b664b2777aa971dfdf9595f07
estar <122713462+estar3266@users.noreply.github.com>
2024-04-23 22:31:19 +0900

    Update game-cm.js

--- js/game-cm.js
+++ js/game-cm.js
@@ -56,8 +56,8 @@ function showInitialModal() {
     const modalImage = document.createElement("img");
     modalImage.src = "img/game-cm/manual.png"; // 여기에 특정 이미지 경로를 지정하세요.
     // modalImage.style.width = "100%";
-    modalImage.style.width = "100%";
-    modalImage.style.height = "auto";
+    modalImage.style.width = "70vw";
+    modalImage.style.height = "70vw";
 
     modalTitle.innerHTML = ''; // 기존 모달 내용을 비웁니다.
     modalTitle.appendChild(modalImage); // 이미지를 모달 타이틀에 추가합니다.
